3ada21954b30bb114ce87197ca70c578de1aa6a9
Lucas Staroscky <staroscky60@gmail.com>
2025-09-08 11:19:20 -0300

    feat: update ESLint configuration and improve linting scripts

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,40 @@
-export default {
-  root: true,
-  languageOptions: {
-    parser: "@typescript-eslint/parser",
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: "module",
-      project: "./tsconfig.json",
+import parser from "@typescript-eslint/parser";
+import * as eslintPlugin from "@typescript-eslint/eslint-plugin";
+
+export default
+  {
+    files: ["*.ts", "*.tsx"],
+    ignores: ["dist/", "node_modules/"],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: "module",
+        project: "./tsconfig.json",
+      },
+      globals: {
+        process: "readonly",
+        __dirname: "readonly",
+        module: "readonly",
+        require: "readonly",
+        // Jest
+        describe: "readonly",
+        test: "readonly",
+        expect: "readonly",
+        beforeEach: "readonly",
+        afterEach: "readonly",
+        jest: "readonly",
+      },
     },
-  },
-  env: {
-    node: true,
-    es2021: true,
-    jest: true,
-  },
-  plugins: {
-    "@typescript-eslint": require("@typescript-eslint/eslint-plugin"),
-  },
-  rules: {
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "no-console": "warn",
-    "semi": ["error", "always"],
-    "quotes": ["error", "double"],
-  },
-  ignorePatterns: ["dist/", "node_modules/"],
-};
+    plugins: {
+      "@typescript-eslint": eslintPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "@typescript-eslint/explicit-function-return-type": "off",
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "no-console": "warn",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  };
